b210830a21a9711c41549ed4aaa82b9270191cef
Simone Gigante <92672883+simoncraf@users.noreply.github.com>
2024-05-12 17:44:28 +0200

    feat: Added networks creation

---
+++ networks.py
@@ -0,0 +1,19 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def create_graph(graph_type: str, **kwargs):
+    """Generate a graph of a given type with the given parameters."""
+    match graph_type:
+        case "barabasi_albert":
+            return nx.barabasi_albert_graph(kwargs.get("n", 500), kwargs.get("m", 2))
+        case "erdos_renyi":
+            return nx.erdos_renyi_graph(kwargs.get("n", 500), kwargs.get("p", 0.1))
+        case "watts_strogatz":
+            return nx.watts_strogatz_graph(kwargs.get("n", 500),kwargs.get("k",4), kwargs.get("p", 0.1))
+        case _:
+            raise ValueError(f"Unknown graph type: {graph_type}")
+        
+def draw_graph(graph):
+    """Draw the given graph."""
+    nx.draw(graph, with_labels=True)
+    plt.show()
